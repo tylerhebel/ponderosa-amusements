5f2cef21d7b1ab98e8c12460b5f8976cf5bce32b
tylerhebel <tyler.hebel@capgemini.com>
2024-06-18 19:53:43 -0500

    More fed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,6 +13,18 @@
         <!-- Styles -->
         <script src="https://cdn.tailwindcss.com"></script>
 
+{{--        <script>--}}
+{{--            tailwind.config = {--}}
+{{--                theme: {--}}
+{{--                    extend: {--}}
+{{--                        fontFamily: {--}}
+{{--                            sans: ['Anton'],--}}
+{{--                        },--}}
+{{--                    },--}}
+{{--                }--}}
+{{--            }--}}
+{{--        </script>--}}
+
         @vite('resources/js/app.js')
     </head>
     <body class="font-sans antialiased dark:bg-black dark:text-white/50">
@@ -26,66 +38,23 @@
 
                     <main class="mt-6">
                         <div class="grid gap-6 lg:grid-cols-2 lg:gap-8">
-                            <img class="" src="{{ Vite::asset('resources/images/logo_black.jpg') }}" />
-
-                            <div class="space-y-4">
-                                <div class="relative gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]">
-                                    <div class="pt-3 sm:pt-5">
-                                        <h2 class="text-xl font-semibold text-black dark:text-white">League 101</h2>
-
-                                    </div>
-
-                                    <div class="flex justify-start">
-                                        <a href="#_" class="inline-flex items-center w-full px-5 py-3 mb-3 mr-1 text-base font-semibold text-white no-underline align-middle bg-blue-600 border border-transparent border-solid rounded-md cursor-pointer select-none sm:mb-0 sm:w-auto hover:bg-blue-700 hover:border-blue-700 hover:text-white focus-within:bg-blue-700 focus-within:border-blue-700">
-                                            Button Text
-                                            <svg class="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
-                                        </a>
-                                        <a href="#_" class="inline-flex items-center w-full px-5 py-3 mb-3 mr-1 text-base font-semibold text-white no-underline align-middle bg-blue-600 border border-transparent border-solid rounded-md cursor-pointer select-none sm:mb-0 sm:w-auto hover:bg-blue-700 hover:border-blue-700 hover:text-white focus-within:bg-blue-700 focus-within:border-blue-700">
-                                            Button Text
-                                            <svg class="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
-                                        </a>
-                                        <a href="#_" class="inline-flex items-center w-full px-5 py-3 mb-3 mr-1 text-base font-semibold text-white no-underline align-middle bg-blue-600 border border-transparent border-solid rounded-md cursor-pointer select-none sm:mb-0 sm:w-auto hover:bg-blue-700 hover:border-blue-700 hover:text-white focus-within:bg-blue-700 focus-within:border-blue-700">
-                                            Button Text
-                                            <svg class="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
-                                        </a>
+                            <div>
+                                <img class="" src="{{ Vite::asset('resources/images/logo_black.jpg') }}" />
+                                <div class="mt-8 rounded-lg bg-white p-2 shadow ring-4 ring-black transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]">
+                                    <div class="">
+                                        <h2 class="font-sans text-2xl font-semibold text-black dark:text-white tracking-wider uppercase">League 101</h2>
                                     </div>
                                 </div>
+                            </div>
 
-                                <a
-                                    href="#"
-                                    class="relative flex items-start gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
-                                >
-                                    <div class="pt-3 sm:pt-5">
-                                        <h2 class="text-xl font-semibold text-black dark:text-white">League 101</h2>
-
+                            <div class="grid grid-cols-2 gap-4">
+                                @for($i = 1; $i <= 14; $i++)
+                                    <div class="rounded-lg bg-white p-2 shadow ring-4 ring-black transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]">
+                                        <div class="">
+                                            <h2 class="font-sans text-2xl font-semibold text-black dark:text-white tracking-wider uppercase">League 101</h2>
+                                        </div>
                                     </div>
-
-                                    <svg class="size-6 stroke-[#FF2D20]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"><path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"/></svg>
-                                </a>
-
-                                <a
-                                    href="#"
-                                    class="relative flex items-start gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
-                                >
-                                    <div class="pt-3 sm:pt-5">
-                                        <h2 class="text-xl font-semibold text-black dark:text-white">League 101</h2>
-
-                                    </div>
-
-                                    <svg class="size-6 stroke-[#FF2D20]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"><path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"/></svg>
-                                </a>
-
-                                <a
-                                    href="#"
-                                    class="relative flex items-start gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
-                                >
-                                    <div class="pt-3 sm:pt-5">
-                                        <h2 class="text-xl font-semibold text-black dark:text-white">League 101</h2>
-
-                                    </div>
-
-                                    <svg class="size-6 stroke-[#FF2D20]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"><path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"/></svg>
-                                </a>
+                                @endfor
                             </div>
                         </div>
                     </main>
@@ -125,11 +94,11 @@
             </div>
         </div>
 
-        <div class="">
-            <button class="modal-button bg-teal-200 p-3 rounded-lg text-teal-900 hover:bg-teal-300">
-                Blast off
-            </button>
-        </div>
+{{--        <div class="">--}}
+{{--            <button class="modal-button bg-teal-200 p-3 rounded-lg text-teal-900 hover:bg-teal-300">--}}
+{{--                Blast off--}}
+{{--            </button>--}}
+{{--        </div>--}}
         <div class="modal opacity-0 pointer-events-none absolute w-full h-full top-0 left-0 flex items-center justify-center">
             <div class="modal-overlay absolute w-full h-full bg-black opacity-25 top-0 left-0 cursor-pointer"></div>
             <div class="absolute w-1/2 h-32 bg-white rounded-sm shadow-lg flex items-center justify-center text-2xl">
